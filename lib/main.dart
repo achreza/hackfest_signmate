4b4062bb824dc6ed3c523a8f1460f92bc1951f06
= <=>
2023-02-26 15:27:09 +0700

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 import 'package:hackfest_signmate/constant.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ScreenUtilInit(
       designSize: Size(394, 916),
       builder: (context, child) {
