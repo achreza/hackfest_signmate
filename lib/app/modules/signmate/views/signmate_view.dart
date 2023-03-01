f7f3141ca35852a4c1507e583713cd19b9637ce6
= <=>
2023-03-02 00:27:23 +0700

    signmate view

--- lib/app/modules/signmate/views/signmate_view.dart
+++ lib/app/modules/signmate/views/signmate_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:hackfest_signmate/constant.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import '../controllers/signmate_controller.dart';
 
@@ -10,14 +13,142 @@ class SignmateView extends GetView<SignmateController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SignmateView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'SignmateView is working',
-          style: TextStyle(fontSize: 20),
+        backgroundColor: primaryColor,
+        title: Text(
+          'SignMate',
+          style: TextStyle(fontWeight: FontWeight.w600),
         ),
+        centerTitle: false,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Heading(),
+            SizedBox(
+              height: 30.h,
+            ),
+            //categories
+            Container(
+              width: Get.width,
+              child: Text(
+                'Place Categories',
+                style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1,
+                    color: Colors.black),
+              ),
+            ),
+            SizedBox(
+              height: 20.h,
+            ),
+            Column(
+              children: [
+                PlaceCategories(context, 'Hospital Care',
+                    'Hospital and clinics', 'health.png'),
+                PlaceCategories(
+                    context, 'Food', 'Restaurants and Cafes', 'food.png'),
+                PlaceCategories(context, 'Entertainment', 'Recreation Places',
+                    'emoticon.png'),
+                PlaceCategories(context, 'Learning Center', 'School Center',
+                    'learning_center.png'),
+                PlaceCategories(
+                    context, 'Other Categories', '', 'other_categories.png'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container PlaceCategories(
+      BuildContext context, String title, String subtitle, String image) {
+    return Container(
+      width: Get.width,
+      height: 90.h,
+      margin: EdgeInsets.only(bottom: 10.h),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: lightColor,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 10.w),
+            width: (Get.width * 0.1).w,
+            child: Image.asset('assets/images/place_categories/${image}'),
+          ),
+          Container(
+            width: (Get.width * 0.4).w,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1),
+                ),
+                subtitle != ''
+                    ? Text(
+                        subtitle,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            letterSpacing: 1),
+                      )
+                    : Container()
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  const Heading({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Get.width,
+      height: 120.h,
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: HexColor("#BFDCA6"),
+          width: 2,
+        ),
+        color: HexColor('#ecf5e4'),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: (Get.width * 0.4).w,
+            child: Text('SignMate to go out with a disabled mate',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 1,
+                    fontSize: 16.sp)),
+          ),
+          Container(
+            width: (Get.width * 0.3).w,
+            child: Image.asset('assets/images/white_skin_handshake.png'),
+          ),
+        ],
       ),
     );
   }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -24,7 +24,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.DASHBOARD;
+  static const INITIAL = Routes.SIGNMATE;
 
   static final routes = [
     GetPage(
